5770a21cb98838d0763c89253dd1c99ceeb56dac
Alexis Cortes <alexis.cortes.0199@gmail.com>
2023-09-19 23:37:20 -0600

    feat(FileCollection.php): custom construct parse data file to Class UploadedFile

--- src/FileCollection.php
+++ src/FileCollection.php
@@ -2,7 +2,27 @@
 
 namespace Yolanda\Http\Request;
 
+use Yolanda\Http\Request\File\File;
+use Yolanda\Http\Request\File\UploadedFile;
+
 class FileCollection extends Collection
 {
 
+    public function __construct(array $inputs = [])
+    {
+        parent::__construct();
+
+        foreach ($inputs as $key => $value) {
+            $this->inputs[$key] =   new UploadedFile($value['tmp_name'], $value['name'], $value['type'], $value['error']);
+        }
+
+    }
+
+    public function get(string $key, mixed $default = null): ?File
+    {
+        if (!array_key_exists($key, $this->inputs)){
+            return null;
+        }
+        return $this->inputs[$key];
+    }
 }
